1f35a1f65d73379150d527828ad3fcbcdbf4e7c2
Eden Bashan <edenbashan@outlook.com>
2023-09-06 21:24:34 +0000

    changed file

--- git-practice.py
+++ git-practice.py
@@ -1,4 +1,12 @@
-first = "Eden"
-last = "Bashan"
+toDoList = ["Math Homework", "Cook Dinner", "Fold Laundry"]
 
-print("My name is", first, last)
+def addItem(item):
+   toDoList.append(item)
+   return toDoList
+
+userAns = input("Do you want to add to your list or quit? A/Q")
+while userAns == "A":
+   item = input("What item do you want to add?")
+   addItem(item)
+   userAns = input("Do you want to add to your list or quit? A/Q")
+print(toDoList)
