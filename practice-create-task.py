0d0d6a2921adc5cefbad7b4800c43111bc581931
Eden Bashan <edenbashan@outlook.com>
2023-09-13 23:36:44 +0000

    added more

--- practice-create-task.py
+++ practice-create-task.py
@@ -1,5 +1,24 @@
 # pokemon battle like game
 
 # format: [name, health, strength, type, vulnerability]
-red = ["torch", 100]
-green = ["tree", 200, 50, "nature", "fire"]
+red = ["fire dog", 80, 60, "fire", "water"]
+blue = ["water gun", 120, 50, "water", "electric"]
+yellow = ["car battery", 60, 70, "electric", "nature"]
+green = ["wisdom tree", 200, 30, "nature", "fire"]
+
+print("Available Creatures: red, blue, yellow, green.")
+
+#c1 = creature 1
+def pick_creature():
+    p1 = input("Player 1, what creature whould you like to pick? ")
+    #p2 = input("Player 2, what creature whould you like to pick? ")
+
+    if p1 == "red":
+        c1 = red
+    elif p1 == "blue":
+        
+
+
+    return c1, c2
+
+def battle(c1, c2):
