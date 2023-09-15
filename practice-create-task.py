32e69be4b45c1fcb85c3886ce86663b10c5e86c2
Eden Bashan <edenbashan@outlook.com>
2023-09-15 15:34:05 +0000

    added more

--- practice-create-task.py
+++ practice-create-task.py
@@ -1,24 +1,44 @@
 # pokemon battle like game
 
 # format: [name, health, strength, type, vulnerability]
-red = ["fire dog", 80, 60, "fire", "water"]
-blue = ["water gun", 120, 50, "water", "electric"]
-yellow = ["car battery", 60, 70, "electric", "nature"]
-green = ["wisdom tree", 200, 30, "nature", "fire"]
+fire_dog = ["Fire Dog", 80, 60, "fire", "water"]
+water_gun = ["Water Gun", 120, 50, "water", "electric"]
+car_battery = ["Car Battery", 60, 70, "electric", "nature"]
+wisdom_tree = ["Wisdom Tree", 200, 30, "nature", "fire"]
 
-print("Available Creatures: red, blue, yellow, green.")
+print("Available Creatures: fire dog, water gun, car battery, wisdom tree.")
 
-#c1 = creature 1
-def pick_creature():
-    p1 = input("Player 1, what creature whould you like to pick? ")
+#c1 stands for creature 1
+def pick_creature(player):
+    choice = input("Player", str(player), "what creature whould you like to pick? ")
     #p2 = input("Player 2, what creature whould you like to pick? ")
 
-    if p1 == "red":
-        c1 = red
-    elif p1 == "blue":
+    if choice == "fire dog":
+        creature = fire_dog
+    elif coice == "water gun":
+        creature = water_gun
+    elif choice == "car battery":
+        creature = car_battery
+    elif choice == "wisdom tree":
+        creature = wisdom_tree
+
+    # if p2 == "fire dog":
+    #     c2 = fire_dog
+    # elif p2 == "water gun":
+    #     c2 = water_gun
+    # elif p2 == "car battery":
+    #     c2 = car_battery
+    # elif p2 == "wisdom tree":
+    #     c2 = wisdom_tree
         
+    return creature
 
-
-    return c1, c2
+    c1 = pick_creature(1)
+    c2 = pick_creature(2)
 
 def battle(c1, c2):
+    print(c1[0], "(Player 1) is fighting", c2[0], "(Player 2).")
+    print("Available Actions: attack, special, block")
+    while game_status == True:
+        while c1[1] > 0 and c2[1] > 0:
+        p1 = input()
