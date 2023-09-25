6fe4459ade8d2bb5c5810f98ab8b06b38212f2fa
Eden Bashan <edenbashan@outlook.com>
2023-09-25 21:39:56 +0000

    added more, almost done

--- practice-create-task.py
+++ practice-create-task.py
@@ -11,31 +11,21 @@ print("Certain elements deal more damage to vulnerable elements. ex: Fire dog do
 
 #c1 stands for creature 1
 def pick_creature(player):
-    choice = input("Player", str(player), "what creature whould you like to pick? ")
-    #p2 = input("Player 2, what creature whould you like to pick? ")
-
+    choice = input("Player " + str(player) + " what creature whould you like to pick? ")
+    
     if choice == "fire dog":
         creature = fire_dog
-    elif coice == "water gun":
+    elif choice == "water gun":
         creature = water_gun
     elif choice == "car battery":
         creature = car_battery
     elif choice == "wisdom tree":
         creature = wisdom_tree
-
-    # if p2 == "fire dog":
-    #     c2 = fire_dog
-    # elif p2 == "water gun":
-    #     c2 = water_gun
-    # elif p2 == "car battery":
-    #     c2 = car_battery
-    # elif p2 == "wisdom tree":
-    #     c2 = wisdom_tree
         
     return creature
 
-    c1 = pick_creature(1)
-    c2 = pick_creature(2)
+c1 = pick_creature(1)
+c2 = pick_creature(2)
 
 def turns(player, choice):
     if player == 1:
@@ -58,10 +48,10 @@ def turns(player, choice):
     elif choice == "special":
         enemy[1] -= damage * 2
     # block
-    elif choice == "block":
-        creature[1] = creature[1]
-        blocked = True
-        return blocked
+    # elif choice == "block":
+    #     creature[1] = creature[1]
+    #     blocked = True
+    #     return blocked
 
     print(enemy[0], "takes", str(damage), "damage")
     return enemy
@@ -69,18 +59,24 @@ def turns(player, choice):
 
 def battle(c1, c2):
     print(c1[0], "(Player 1) is fighting", c2[0], "(Player 2).")
-    print("Available Actions: attack, special, block")
+    print("Available Actions: attack, special")
     print("special has 2 uses but does 2x normal damage")
 
 
-
     while c1[1] > 0 and c2[1] > 0:
-        p1 = input("What would you like to do? (options: attack, special, block)")
-        p2 = input("What would you like to do? (options: attack, special, block)")
+        p1 = input("Player 1, what would you like to do? (options: attack, special) ")
+        p2 = input("Player 2, what would you like to do? (options: attack, special) ")
 
         c2 = turns(1, p1)
         c1 = turns(2, p2)
 
         print("STATUS:")
-        print("player 1 health -", str(c1[1]))
-        print("player 2 health -", str(c2[1]))
+        print("Player 1 has ", str(c1[1]), "health")
+        print("Player 2 has ", str(c2[1]), "health")
+    
+    if c1[1] > 0:
+        print("Player 1 wins")
+    elif c2[1] > 0:
+        print("Player 2 wins")
+
+battle(c1, c2)
